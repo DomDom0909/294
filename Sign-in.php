a60a4b02e2a91c8da27fead948e04601486bef6b
DESKTOP-GH0KSAU\domin <dominic.bannert@gmail.com>
2022-11-24 08:33:58 +0100

    Transfer Code to Github

---
+++ Sign-in.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <title>LOGIN</title>
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+
+<body>
+
+    <form action="Sign-in.php" method="post">
+
+        <h2>LOGIN</h2>
+
+        <?php if (isset($_GET['error'])) { ?>
+
+            <p class="error"><?php echo $_GET['error']; ?></p>
+
+        <?php } ?>
+
+        <label>User Name</label>
+
+        <input type="text" name="uname" placeholder="User Name"><br>
+
+        <label>Password</label>
+
+        <input type="password" name="password" placeholder="Password"><br> 
+
+        <button type="submit">Login</button>
+    </form>
+
+</body>
+
+</html>
